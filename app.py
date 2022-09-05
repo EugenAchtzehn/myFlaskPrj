b1bcd2e9c6945dc0158be5b884ff0115e6204441
EugenAchtzehn <eugene1826@gmail.com>
2022-09-06 01:45:25 +0800

    [Add] form templates to test how backend handle data.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -40,6 +40,29 @@ def pageData():
   }
   return render_template('page.html', data=data)
 
+# 在模板頁面中引入 js, css
 @app.route('/static')
 def staticPage():
   return render_template('static.html')
+
+@app.route('/form')
+def formPage():
+  return render_template('form.html')
+
+@app.route('/submit', methods=['POST', 'GET'])
+def submit():
+  if request.method == 'POST':
+    user = request.form["user"]
+    print("post: user => ", user)
+    return redirect(url_for("success", name=user, action="post"))
+  else:
+    user = request.args.get("user")
+    print("get : user => ", user)
+    return redirect(url_for("success", name=user, action="get"))
+
+@app.route("/success/<action>/<name>")
+def success(name, action):
+    return "{} : Welcome {} ~ !!!".format(action, name)
+
+if __name__ == "__main__":
+    app.run()
